c455374c6a8ad31a38e706d8598a3f0e832e3255
Keith Morrow <keith@3d3.ca>
2013-02-02 16:53:27 -0500

    Added highlighting support on pastes

--- 3d3client.js
+++ 3d3client.js
@@ -4,6 +4,16 @@ var sjcl = require('./sjcl.js');
 var http = require('http');
 var args = require('optimist').argv;
 
+eMap = {
+    ruby: 'rb', python: 'py', perl: 'pl', php: 'php', scala: 'scala', go: 'go',
+    xml: 'xml', html: 'xml', htm: 'xml', css: 'css', javascript: 'js', vbscript: 'vbs',
+    lua: 'lua', delphi: 'pas', pascal: 'pas', java: 'java', cpp: 'cpp', cc: 'cpp',
+    objectivec: 'm', vala: 'vala', cs: 'cs', csharp: 'cs', sql: 'sql', smalltalk: 'sm',
+    lisp: 'lisp', ini: 'ini', diff: 'diff', bash: 'bash', sh: 'bash', tex: 'tex',
+    erlang: 'erl', haskell: 'hs', markdown: 'md', txt: 'txt', coffee: 'coffee',
+    coffeescript: 'coffee', json: 'json', c: 'cpp', py: 'py', pl: 'pl'
+}
+
 if (args._.length > 0) { // Paste read mode
     var url_in = args._[0].split('/').pop(); // Only get the end of the string
     if (url_in.indexOf("#") != -1) { // Passworded
@@ -71,6 +81,14 @@ else { // Paste mode
             res.on('end', function() {
                 data_out = JSON.parse(data_out);
                 var res_url = "http://3d3.ca/"+data_out['key'];
+                if ('h' in args) {
+                    if (args['h'] in eMap) {
+                        res_url += "."+eMap[args['h']];
+                    }
+                    else {
+                        res_url += ".txt";
+                    }
+                }
                 if ('p' in args) {
                     res_url += "#"+args['p'];
                 }
